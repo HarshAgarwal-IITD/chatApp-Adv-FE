5609894f60235a226eb303ebece474c8b5718630
HARSH AGARWAL <harshagarwal@HARSHs-MacBook-Air-438.local>
2025-03-16 16:09:31 +0530

    ws url update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@ export const BACKEND_URL="https://chatapp-advbe-production.up.railway.app/";
 export const SIGNUP_ROUTE='api/auth/signup';
 export const SIGNIN_ROUTE='api/auth/login';
 export const SIGNOUT_ROUTE='api/auth/logout';
-export const WEBSOCKET_URL='ws://localhost:3000';
+export const WEBSOCKET_URL='wss://chatapp-advbe-production.up.railway.app';
 export const ROOM_ROUTE='api/rooms/';
 export const GOOGLE_SIGNIN_ROUTE = "api/auth/google-signin"
 
